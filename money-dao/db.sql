7c7d2fcac25e3914f94ba1abcbd149ae8a2d9be7
ice <11635@ice>
2017-11-07 01:50:44 +0800

    初建项目

---
+++ money-dao/db.sql
@@ -0,0 +1,22 @@
+SET GLOBAL innodb_file_per_table=1;
+SET GLOBAL innodb_file_format=Barracuda;
+
+CREATE TABLE db_usr(
+  `id` INT(8) NOT NULL AUTO_INCREMENT,
+  `phone` CHAR(13) NOT NULL COMMENT '电话号码',
+  `account` VARCHAR(20) DEFAULT NULL COMMENT '用户账号',
+  `usr_name` VARCHAR(50) DEFAULT NULL COMMENT '用户名',
+  `pwd` VARCHAR(150) DEFAULT NULL COMMENT '密码',
+  `role` INT(2) COMMENT '角色',
+  `identity` CHAR(18) DEFAULT NULL COMMENT '身份证号码',
+  `identity_status` INT(2) COMMENT '身份证状态（0，未认证, 1,正常）',
+  `email` VARCHAR(50) DEFAULT NULL COMMENT '邮箱',
+  `pic_big` VARCHAR(100) DEFAULT NULL COMMENT '大图片路径',
+  `pic_small` VARCHAR(100) DEFAULT NULL COMMENT '小图片路径',
+  `create_time` DATETIME  NOT NULL ,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone` (`phone`)
+)ENGINE=INNODB DEFAULT CHARSET=utf8 KEY_BLOCK_SIZE=4 ROW_FORMAT=COMPRESSED DATA DIRECTORY = '/opt/ice/mysql/innodb'
+
+
+SELECT VERSION();
---
+++ money-dao/src/main/java/com/ice/dao/bo/UsrBo.java
@@ -0,0 +1,136 @@
+package com.ice.dao.bo;
+
+import java.util.Date;
+
+public class UsrBo {
+	/** 主键. */
+	private Integer id;
+	/** 电话号码. */
+	private String phone;
+	/** 账号. */
+	private String account;
+	/** 用户名. */
+	private String usrName;
+	/** 密码. */
+	private String pwd;
+	/** 角色. */
+	private Integer role;
+	/** 身份证号. */
+	private String identity;
+	/** 身份证状态. */
+	private int identityStatus;
+	/** 邮箱. */
+	private String email;
+	/** 电站图片(大图). */
+	public String picBig;
+	/** 电站图片(小图). */
+	public String picSmall;
+	/** 创建时间. */
+	private Date createTime;
+	/** 更新时间. */
+	private Date updateTime;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone == null ? null : phone.trim();
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account == null ? null : account.trim();
+	}
+
+	public String getUsrName() {
+		return usrName;
+	}
+
+	public void setUsrName(String usrName) {
+		this.usrName = usrName == null ? null : usrName.trim();
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd == null ? null : pwd.trim();
+	}
+
+	public Integer getRole() {
+		return role;
+	}
+
+	public void setRole(Integer role) {
+		this.role = role;
+	}
+
+	public String getIdentity() {
+		return identity;
+	}
+
+	public void setIdentity(String identity) {
+		this.identity = identity == null ? null : identity.trim();
+	}
+
+	public int getIdentityStatus() {
+		return identityStatus;
+	}
+
+	public void setIdentityStatus(int identityStatus) {
+		this.identityStatus = identityStatus;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email == null ? null : email.trim();
+	}
+
+	public String getPicBig() {
+		return picBig;
+	}
+
+	public void setPicBig(String picBig) {
+		this.picBig = picBig;
+	}
+
+	public String getPicSmall() {
+		return picSmall;
+	}
+
+	public void setPicSmall(String picSmall) {
+		this.picSmall = picSmall;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+}
